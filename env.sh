dea6a898732c5f31da1fe3842c0a2e2c0b9c477a
Wasseem Khayrattee <hey@wk.contact>
2024-12-21 17:50:59 +0400

    chore(env): add WP Engine placeholders and GitHub repo details
    - Introduce placeholders for GitHub repo and WP Engine (dev, stag, prod) repos
    - Export SSH credentials for WP Engine environments
    - Include comments prompting users to adjust values for their context
    - Keep paths and file-deletion list consistent with existing script structure

--- env.sh
+++ env.sh
@@ -7,21 +7,43 @@
 # Typically, this file is sourced by header.sh (which sets SCRIPT_DIR first).
 #
 # Exports:
-#   TMP_FOLDER_NAME       : Name for temporary folder
-#   BLOG_REPO_NAME        : Name of the blog repository
-#   WP_ENV_PATH           : Path to the .env file within WordPress
-#   WP_ENV_DIR            : Directory that holds WordPress environment config
-#   GPG_PARAPHRASE_PATH   : Path to the user's GPG paraphrase file
+#   TMP_FOLDER_NAME         : Temporary folder name
+#   BLOG_REPO_NAME          : Name of the blog repository
+#   WP_ENV_PATH             : Path to the .env file within WordPress
+#   WP_ENV_DIR              : Directory that holds WordPress environment config
+#   GPG_PARAPHRASE_PATH     : Path to the user's GPG paraphrase file
 #   list_of_files_to_delete : Array of files/directories to remove prior to deploy
-#   PATH_TO_TMP_FOLDER    : Absolute path to the temporary folder
-#   PATH_TO_BLOG_REPO     : Absolute path to the blog repository within the temp folder
-#   PATH_TO_BLOG_ENV      : Absolute path to the blog environment config
-#   PATH_TO_GITIGNORE_WPE : Absolute path to the WP Engine gitignore template
-#   THIS_TIMESTAMP        : Current timestamp in the format "YYYY.MM.DD-HHMMSS"
+#   PATH_TO_TMP_FOLDER      : Absolute path to the temporary folder
+#   PATH_TO_BLOG_REPO       : Absolute path to the blog repository within the temp folder
+#   PATH_TO_BLOG_ENV        : Absolute path to the blog environment config
+#   PATH_TO_GITIGNORE_WPE   : Absolute path to the WP Engine gitignore template
+#   THIS_TIMESTAMP          : Current timestamp in the format "YYYY.MM.DD-HHMMSS"
+#   BLOG_GIT_REPO           : GitHub repository URL (placeholder)
+#   WPE_GIT_DEV/STAG/PROD   : WP Engine Git repo URLs (placeholders)
+#   WPE_SSH_DEV/STAG/PROD   : WP Engine SSH credentials (placeholders)
 #
 # Author:  Wasseem Khayrattee
 # GitHub:  https://github.com/wkhayrattee
 #
+#------------------------------------------------------------------------------
+
+#------------------------------------------------------------------------------
+# GITHUB & WP ENGINE REPOSITORY/SSH PLACEHOLDERS
+#------------------------------------------------------------------------------
+# TODO: Adjust these values to match your own GitHub & WP Engine environments.
+## Your Project's Git repo
+export BLOG_GIT_REPO="git@github.com:YOURORG/YOURPROJECT.git"
+## WP Engine Git repos - DEV, STAG, PROD
+export WPE_GIT_DEV="git@git.wpengine.com:production/ENVdevblog.git"
+export WPE_GIT_STAG="git@git.wpengine.com:production/ENVstag.git"
+export WPE_GIT_PROD="git@git.wpengine.com:production/ENVprod.git"
+## WP Engine SSH credentials - DEV, STAG, PROD
+export WPE_SSH_DEV="ssh ENVdevblog@ENVdevblog.ssh.wpengine.net"
+export WPE_SSH_STAG="ssh ENVstag@ENVstag.ssh.wpengine.net"
+export WPE_SSH_PROD="ssh ENVprod@ENVprod.ssh.wpengine.net"
+
+#------------------------------------------------------------------------------
+# BASIC VARIABLES
 #------------------------------------------------------------------------------
 export TMP_FOLDER_NAME='tmp'
 export BLOG_REPO_NAME='wpblog'
@@ -47,9 +69,9 @@ export list_of_files_to_delete=(
   "wp-content/app/config/test_env"
 )
 
-# ------------------------------------------------------------------------------
-# Paths that depend on SCRIPT_DIR (set in header.sh before env.sh is sourced)
-# ------------------------------------------------------------------------------
+#------------------------------------------------------------------------------
+# DEPENDENT PATHS (Require SCRIPT_DIR from header.sh)
+#------------------------------------------------------------------------------
 export PATH_TO_TMP_FOLDER="${SCRIPT_DIR}/${TMP_FOLDER_NAME}"
 export PATH_TO_BLOG_REPO="${PATH_TO_TMP_FOLDER}/${BLOG_REPO_NAME}"
 export PATH_TO_BLOG_ENV="${PATH_TO_BLOG_REPO}/wp-content/app/config/"
